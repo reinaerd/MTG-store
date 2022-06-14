08e0ab1255bb90ea0754a0a8e41eb0208b69807f
reinaerd <58473900+reinaerd@users.noreply.github.com>
2022-06-14 11:02:40 +0200

    Add files via upload

---
+++ mtg-lib-main (1)/mtg-lib-main/Library/Models/CardType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mtg_lib.Library.Models
+{
+    public partial class CardType
+    {
+        public long Id {get; set; }
+        public long CardId { get; set; }
+        public long TypeId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public virtual Card Card { get; set; } = null!;
+        public virtual Type Type { get; set; } = null!;
+    }
+}
---
+++ mtg-lib-main (1)/mtg-lib-main/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using mtg_lib.Library.Models;
+namespace password 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MTGdatabaseContext context = new MTGdatabaseContext();
+            
+            var cardsList = context.Cards
+                    .OrderBy(c => c.Name)
+                    .ToList();
+            
+            foreach(var item in cardsList){
+                Console.WriteLine(item.Name);
+            }
+        }
+    }
+}
